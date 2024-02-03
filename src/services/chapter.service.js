bd90f3d560d1f34986a43382c2d026ef538b9792
dafariski77 <rskdafa@gmail.com>
2024-02-04 03:25:19 +0700

    feat(chapter): add service for crud

---
+++ src/services/chapter.service.js
@@ -0,0 +1,54 @@
+import prisma from "../lib/prisma.js";
+
+const getAlls = async () => {
+  const chapters = await prisma.chapter.findMany();
+
+  return chapters;
+};
+
+const create = async (data) => {
+  const chapter = await prisma.chapter.create({
+    data,
+  });
+
+  return chapter;
+};
+
+const getOne = async (chapterId) => {
+  const chapter = await prisma.chapter.findFirstOrThrow({
+    where: {
+      id: chapterId,
+    },
+  });
+
+  return chapter;
+};
+
+const update = async (chapterId, data) => {
+  const chapter = await prisma.chapter.update({
+    where: {
+      id: chapterId,
+    },
+    data,
+  });
+
+  return chapter;
+};
+
+const remove = async (chapterId) => {
+  const chapter = await prisma.chapter.delete({
+    where: {
+      di: chapterId,
+    },
+  });
+
+  return;
+};
+
+export default {
+  getAlls,
+  getOne,
+  create,
+  update,
+  remove,
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 export { default as authService } from "./auth.service.js";
 export { default as userService } from "./user.service.js";
+export { default as chapterService } from "./chapter.service.js";
