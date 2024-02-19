4e47988b09376f15eb8d7a47032d797104202de3
dafariski77 <rskdafa@gmail.com>
2024-02-19 18:41:57 +0700

    fix(schema): update sql migration

---
+++ prisma/migrations/20240219114114_remove_quiz_id_on_quiz_option/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quizId` on the `QuizOption` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "QuizOption" DROP COLUMN "quizId";
