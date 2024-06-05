bcef742bd93fe4b75f6bc091cf00e26e33d378cf
dafariski77 <rskdafa@gmail.com>
2024-06-06 00:27:13 +0700

    fix(server): fixing server host

--- src/index.js
+++ src/index.js
@@ -43,8 +43,6 @@ app.use(handleNotFoundRoute);
 // const server = https.createServer(options, app);
 const server = http.createServer(app);
 
-server.listen(port);
-
 server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
