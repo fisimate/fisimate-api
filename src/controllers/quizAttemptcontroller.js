11e4116a3ea7e1d34074de44b19079861b93d36f
dafariski77 <rskdafa@gmail.com>
2024-06-30 13:50:52 +0700

    feat(quiz): add quiz logic

---
+++ src/controllers/quizAttemptcontroller.js
@@ -0,0 +1,104 @@
+import prisma from "../lib/prisma.js";
+import apiSuccess from "../utils/apiSuccess.js";
+
+const getAllAttempts = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const attempts = await prisma.quizAttempt.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        quiz: true,
+        userQuizResponse: true,
+      },
+    });
+
+    return apiSuccess(res, "Berhasil mendapatkan data!", attempts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAttemptByQuizId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const attempt = await prisma.quizAttempt.findUnique({
+      where: { id },
+      include: {
+        quiz: {
+          include: {
+            questions: {
+              include: {
+                quizOptions: true,
+              },
+            },
+          },
+        },
+        userQuizResponse: true,
+      },
+    });
+
+    return apiSuccess(res, "Berhasil mendapatkan data!", attempt);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create attempt
+const createAttempt = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { responses } = req.body;
+
+    // Save user responses
+    const responsePromises = responses.map(async (response) => {
+      const { questionId, selectedOptionId } = response;
+      return prisma.userQuizResponse.create({
+        data: {
+          quizAttemptId: attemptId,
+          questionId,
+          selectedOptionId,
+        },
+      });
+    });
+
+    const savedResponses = await Promise.all(responsePromises);
+
+    // Calculate the number of correct responses
+    const correctResponses = await prisma.userQuizResponse.findMany({
+      where: {
+        quizAttemptId: attemptId,
+        selectedOption: { isCorrect: true },
+      },
+    });
+
+    // Assuming each quiz has exactly 4 questions
+    const totalQuestions = 4;
+    const correctCount = correctResponses.length;
+
+    // Calculate the score out of 100
+    const score = (correctCount / totalQuestions) * 100;
+
+    // Update the score of the quiz attempt
+    await prisma.quizAttempt.update({
+      where: { id: attemptId },
+      data: { score },
+    });
+
+    return apiSuccess(res, "Berhasil menjawab kuis!", {
+      savedResponses,
+      score,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getAllAttempts,
+  getAttemptByQuizId,
+  createAttempt,
+};
