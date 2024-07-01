1ebf2d6a76fd1470e5a5110938902583537bf04a
dafariski77 <rskdafa@gmail.com>
2024-07-02 03:11:22 +0700

    feat(bank): add api crud for examBank

--- src/routes/examBank.route.js
+++ src/routes/examBank.route.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { examBankController } from "../controllers/index.js";
 import { authenticateUser } from "../middlewares/auth.js";
+import upload from "../lib/multer.js";
 
 const router = express.Router();
 
-router.get("/", authenticateUser, examBankController.index);
+router.get(
+  "/",
+  authenticateUser,
+  upload.fields([{ name: "icon" }, { name: "fileBankPath" }]),
+  examBankController.index
+);
 router.get("/:id", authenticateUser, examBankController.show);
+router.post("/", authenticateUser, examBankController.create);
+router.put("/:id", authenticateUser, examBankController.update);
+router.delete("/:id", authenticateUser, examBankController.destroy);
 
 export default router;
