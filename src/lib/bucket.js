7c7af2398ab031935ccf905d8881fd874cf59217
dafariski77 <rskdafa@gmail.com>
2024-08-21 13:12:58 +0700

    fix: revert to previous change

--- src/lib/bucket.js
+++ src/lib/bucket.js
@@ -1,9 +1,7 @@
 import { Storage } from "@google-cloud/storage";
 import configs from "../configs/index.js";
 
-const storage = new Storage({
-  keyFilename: "./src/secrets/key.json",
-});
+const storage = new Storage();
 
 const bucket = storage.bucket(configs.bucketName);
 
